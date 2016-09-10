4374fbe431073ef22b52d82a3970a789c7efff7e
klee0589 <klee0589@gmail.com>
2016-09-10 16:45:00 -0400

    commit

--- js/contactForm.js
+++ js/contactForm.js
@@ -57,77 +57,13 @@ $(function () {
                 // Set the message text.
                 if (data.responseText !== '') {
                     $(formMessages).text(data.responseText);
+                    console.log(data)
                 } else {
                     $(formMessages).text('Oops! An error occured and your message could not be sent.');
                 }
 
 
                 $("#btn-submit").removeClass("btn-loading");
-            });
-
-    });
-
-});
-
-
-$(function () {
-
-
-    // Get the form.
-    var form = $('#newsletter-form'),
-        // Get the messages div.
-        formMessages = $('#newsletter-messages');
-
-    // Set up an event listener for the contact form.
-    $(form).submit(function (e) {
-        // Stop the browser from submitting the form.
-        e.preventDefault();
-
-
-        $("#btn-newsletter").addClass("btn-loading");
-
-
-        // Serialize the form data.
-        var formData = $(form).serialize();
-
-        // Submit the form using AJAX.
-        $.ajax({
-                type: 'POST',
-                url: $(form).attr('action'),
-                data: formData
-            })
-            .done(function (response) {
-                // Make sure that the formMessages div has the 'success' class.
-                $(formMessages).removeClass('error');
-                $(formMessages).addClass('success');
-                setTimeout(function () {
-                    $(formMessages).addClass("fade-outy");
-                }, 5000);
-
-                // Set the message text.
-                $(formMessages).text(response);
-
-                // Clear the form.
-                $(form).trigger("reset");
-                $("#btn-newsletter").removeClass("btn-loading");
-            })
-            .fail(function (data) {
-                // Make sure that the formMessages div has the 'error' class.
-                $(formMessages).removeClass('success');
-                $(formMessages).addClass('error');
-                setTimeout(function () {
-                    $(formMessages).addClass("fade-outy");
-                }, 5000);
-
-                // Set the message text.
-                if (data.responseText !== '') {
-                    $(formMessages).text(data.responseText);
-                } else {
-                    $(formMessages).text('Oops! An error occured and your message could not be sent.');
-                }
-
-
-                $("#btn-newsletter").removeClass("btn-loading");
             });
 
     });
